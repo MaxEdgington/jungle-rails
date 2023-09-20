e65bad9f2e88779dba6c058628c6603639dca18f
Max Edgington <edgington.m.w@gmail.com>
2023-09-20 14:40:53 -0400

    add a fix that properly displays prices in the erb files

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
 
   def show
+    ## raise "here" to throw an error - whatever typed in is displayed as an error, a natural breakpoint 
     @category = Category.find(params[:id])
+    ## raise.@category.inspect - safely call on any object which turns into dev friendly string for inspection 
     @products = @category.products.order(created_at: :desc)
   end
 
